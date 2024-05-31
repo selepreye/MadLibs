3c6e807cc44ab8b21675a5e2c94f07b99d68ae8c
Selepreye <donovanogbomah@gmail.com>
2024-05-31 15:16:31 +0100

    Initial commit

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+noun = input("Noun:")
+verb = input("Verb:")
+noun2 = input("Noun (plural):")
+noun3 = input("Noun (plural):")
+verb2 = input("Verb (-ing):")
+noun4 = input("Noun (plural):")
+
+
+madlibs = (f"Hello everyone, my name is {noun}. I love to {verb} with {noun2}, {noun3} and also like {verb2}"
+           f" with {noun4}.")
+
+print(madlibs)
